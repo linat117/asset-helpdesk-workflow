672ceed7b900e3edd71b2e6bfff980a48eaff4d8
Hilina Teshome <teshilina8@gmail.com>
2025-08-14 18:13:17 +0300

    Add Employee models -employee and department

--- asset_helpdesk_workflow/settings.py
+++ asset_helpdesk_workflow/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'asset_helpdesk_workflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'asset-helpdesk-db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length= 100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+class Employee(models.Model):
+    
+    #POSITION_CHOICES = [
+     #   ('it_technician', 'It Technician'),
+     #   ('finance_manager', 'Finance Manager'),
+    #    ('customer_service', 'Customer Service'),
+    #]
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('it_head', 'IT Head'),
+        ('it_staff', 'IT Staff'),
+        ('employee', 'Employee'),
+    ]
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    #position = models.CharField(choices= POSITION_CHOICES, max_length=20)
+    position = models.CharField( max_length=200)
+    role = models.CharField(choices=ROLE_CHOICES, max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.role}) - {self.position} from {self.department} department"
