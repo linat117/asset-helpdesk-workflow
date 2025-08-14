89c067e620e030e0d5871e9c0a4da60ec559f0da
Hilina Teshome <teshilina8@gmail.com>
2025-08-14 18:37:41 +0300

    Register employee app models to admin

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Department, Employee
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
-# Register your models here.
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','email','position', 'role', 
+                    'department')
+    search_fields = ('first_name','last_name','email','position', 'role', 
+                    'department')
+    
