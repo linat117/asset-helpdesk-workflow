1bef8dc41e9564376e513c08ba00f636feef24d2
Hilina Teshome <teshilina8@gmail.com>
2025-08-16 12:49:17 +0300

    Add class based views for TicketCategory model

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .models import Ticket, TicketCategory, TicketComment, TicketUpdateLog
+#ticket category crud views
+class TicketCategoryListView(ListView):
+    model = TicketCategory
+    template_name = 'ticketcategories/ticketcategory_list.html'
+    context_object_name = 'ticketcategories'
 
-# Create your views here.
+class TicketCategoryDetailView(DetailView):
+    model = TicketCategory
+    template_name = 'ticketcategories/ticketcategory_detail.html'
+    context_object_name = 'ticketcategory'
+
+class TicketCategoryCreateView(CreateView):
+    model = TicketCategory
+    fields = '__all__'
+    template_name = 'ticketcategories/ticketcategory_form.html'
+    success_url = reverse_lazy('ticket-category-list')
+
+class TicketCategoryUpdateView(UpdateView):
+    model = TicketCategory
+    fields = '__all__'
+    template_name= 'ticketcategories/tickecategory_form.html'
+    success_url = reverse_lazy('ticket-category-list')
+
+class TicketCategoryDeleteView(DeleteView):
+    model = TicketCategory
+    template_name = 'ticketcategories/ticketcategory_delete_confirm.html'
+    success_url = reverse_lazy('ticket-category-list')
