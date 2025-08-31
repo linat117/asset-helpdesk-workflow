3f48027d457307eee5ae71372b56d1e405367437
Hilina Teshome <teshilina8@gmail.com>
2025-08-31 19:51:13 +0300

    update build

--- build.sh
+++ build.sh
@@ -16,8 +16,9 @@ pip install djangorestframework-simplejwt==5.3.0
 pip install gunicorn==21.2.0
 pip install whitenoise==6.6.0
 pip install dj-database-url==2.1.0
-pip install psycopg2-binary==2.9.9
+pip install psycopg2==2.9.9
 pip install python-decouple==3.8
+
 
 echo "=== Checking Django Installation ==="
 python -c "import django; print(f'Django version: {django.get_version()}')"
